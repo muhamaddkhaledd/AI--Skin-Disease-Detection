a1324a43212f45805202b9b79081247eb3795d40
muhamaddkhaledd <mohkhaled6100@gmail.com>
2025-03-27 17:53:52 +0200

    frist commit

---
+++ android/app/src/main/kotlin/com/example/ai_powered_skin_disease_detection_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ai_powered_skin_disease_detection_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:ai_powered_skin_disease_detection_application/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+      ),
+      home: camera(),
+    );
+  }
+}
